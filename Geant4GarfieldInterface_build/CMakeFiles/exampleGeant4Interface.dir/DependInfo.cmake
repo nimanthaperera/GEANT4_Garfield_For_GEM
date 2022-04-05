fc93c12de6becd9d0fcda2a986fddcf2e9335b1b
Nimantha Perera <nimantha_08@hotmail.com>
2022-04-05 12:49:20 +0200

    Initial files from GEANT4 example on GEANT4 Garfield Integration

---
+++ Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/exampleGarfield.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/exampleGarfield.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldDetectorConstruction.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldDetectorConstruction.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldEventAction.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldEventAction.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldG4FastSimulationModel.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldG4FastSimulationModel.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldMessenger.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldMessenger.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldPhysics.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldPhysics.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldPhysicsList.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldPhysicsList.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldPrimaryGeneratorAction.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldPrimaryGeneratorAction.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldRunAction.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldRunAction.cc.o"
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/src/GarfieldSteppingAction.cc" "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/src/GarfieldSteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  "VECCORE_ENABLE_VC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/afs/cern.ch/work/n/nperera/g4_garfield_alfredo/Geant4GarfieldInterface/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/LCG_100/Geant4/10.07.p01/x86_64-centos7-gcc10-opt/include/Geant4"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc10-opt/include/QtCore"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc10-opt/./mkspecs/linux-g++"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc10-opt/include/QtGui"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc10-opt/include/QtWidgets"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc10-opt/include/QtOpenGL"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc10-opt/include/QtPrintSupport"
+  "/cvmfs/sft.cern.ch/lcg/releases/LCG_100/ROOT/v6.24.00/x86_64-centos7-gcc10-opt/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Geant4GarfieldInterface_build/CMakeFiles/exampleGeant4Interface.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleGeant4Interface.dir/exampleGarfield.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldDetectorConstruction.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldEventAction.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldG4FastSimulationModel.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldMessenger.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldPhysics.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldPhysicsList.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldRunAction.cc.o"
+  "CMakeFiles/exampleGeant4Interface.dir/src/GarfieldSteppingAction.cc.o"
+  "exampleGeant4Interface"
+  "exampleGeant4Interface.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleGeant4Interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
